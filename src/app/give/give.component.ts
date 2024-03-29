d69c74e1603df739331e7b57f7cad4057c943171
Andrew Peterson <andrewp2@umbc.edu>
2024-03-29 18:16:16 -0400

    create base components

---
+++ src/app/give/give.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-give',
+  standalone: true,
+  imports: [],
+  templateUrl: './give.component.html',
+  styleUrl: './give.component.scss'
+})
+export class GiveComponent {
+
+}
---
+++ src/app/partnerships/partnerships.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partnerships',
+  standalone: true,
+  imports: [],
+  templateUrl: './partnerships.component.html',
+  styleUrl: './partnerships.component.scss'
+})
+export class PartnershipsComponent {
+
+}
