d43e181b769bb526de72564e72c744435adefbf7
Andrew Peterson <andrewp2@umbc.edu>
2024-03-29 19:33:58 -0400

    adds working navbar, need to fix hamburger

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CurrentEventsComponent } from './current-events/current-events.component';
+import { GiveComponent } from './give/give.component';
+import { GuatemalaPartnershipComponent } from './guatemala-partnership/guatemala-partnership.component';
+import { HomeComponent } from './home/home.component';
+import { LegalComponent } from './legal/legal.component';
+import { OurGuyInGuateComponent } from './our-guy-in-guate/our-guy-in-guate.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PartnershipsComponent } from './partnerships/partnerships.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+
+
+
+
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'current-events', component: CurrentEventsComponent},
+    {path: 'give', component: GiveComponent},
+    {path: 'guatemala', component: GuatemalaPartnershipComponent},
+    {path: 'legal', component: LegalComponent},
+    {path: 'ogg', component: OurGuyInGuateComponent},
+    {path: 'partnerships', component: PartnershipsComponent},
+    {path: 'about', component: ThankYouComponent},
+    {path: '**', component: PageNotFoundComponent},
+
+];
